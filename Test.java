0e6df79ce64c8805cb098c7044fd32e2864d92a8
bisnin0 <dustmq299@gmail.com>
2020-07-16 19:05:11 +0900

    first commit

---
+++ Test.java
@@ -0,0 +1,37 @@
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		int x = 2;
+		int y = 5;
+		char c = 'A';
+		
+		System.out.println(1+x << 33);
+		System.out.println(y>=5 || x<0 && x>2);
+		
+
+		int num = 456;
+		System.out.println((int)(num*0.01)*100);
+
+		byte a = 10;
+		byte b = 20;
+		int c = a+b;
+		System.out.print(c);
+		float f = 3/2f;
+		System.out.println(f);
+		long i =3000*3000*3000;
+		System.out.println(i);
+		float f2 = 0.1f;
+		double d = 0.1;
+		boolean result = d==f2;
+		System.out.println(result);
+*/
+		char ch ='^';
+		boolean b = 'a'<=ch || ch <= 'A' ;
+		System.out.println(b);
+		System.out.println();
+
+
+	}
+}
